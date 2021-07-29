f16c9abedc2abb8af825f18b7c9de6f5920226cf
marcchew <73992781+marcchew@users.noreply.github.com>
2021-07-29 22:56:28 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+let lightTheme = "light.css";
+let darkTheme = "dark.css";
+
+// Clears the screen on click of C button.
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+// Displays entered value on screen.
+function liveScreen(value) {
+  let res = document.getElementById("result");
+  if(res.value == "undefined"){
+    res.value = "";
+  }
+  res.value += value;
+}
+// Swaps the style sheet in order to  achieve dark mode.
+function changeTheme() {
+  let darkMode = document.getElementById("dark-mode");
+  let theme = document.getElementById("theme");
+  if (theme.getAttribute("href") == lightTheme) {
+    theme.href = darkTheme;
+    darkMode.innerHTML = "Light Mode 🌞";
+  } else {
+    theme.href = lightTheme;
+    darkMode.innerHTML = "Dark Mode 🌙";
+  }
+}
